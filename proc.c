b0806fc5dd09ad199c751897f4336dc457deb7fc
watermelondududu <879761184@qq.com>
2017-12-25 19:01:25 +0800

    實現 priority scheduling

--- proc.c
+++ proc.c
@@ -88,7 +88,7 @@ allocproc(void)
 found:
   p->state = EMBRYO;
   p->pid = nextpid++;
-
+  p->priority = 10; //default priority
   release(&ptable.lock);
 
   // Allocate kernel stack.
@@ -323,6 +323,8 @@ void
 scheduler(void)
 {
   struct proc *p;
+	struct proc *p1;
+
   struct cpu *c = mycpu();
   c->proc = 0;
   
@@ -330,21 +332,28 @@ scheduler(void)
     // Enable interrupts on this processor.
     sti();
 
+		struct proc *highP;
+
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state != RUNNABLE)
         continue;
-
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
-
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
+			
+			highP=p;
+			// choose one with highest priority
+			for(p1=ptable.proc;p1<&ptable.proc[NPROC];p1++){
+				if(p1->state!=RUNNABLE)
+					continue;
+				if(highP->priority>p1->priority)
+					highP=p1;
+			}
+			p=highP;
+		  c->proc=p;
+			switchuvm(p);
+			p->state=RUNNING;
+			swtch(&(c->scheduler),p->context);
+			switchkvm();
 
       // Process is done running for now.
       // It should have changed its p->state before coming back.
@@ -531,4 +540,47 @@ procdump(void)
     }
     cprintf("\n");
   }
+}
+
+//cps
+int
+cps()
+{
+	struct proc *p;
+	
+	//enable interrupts on this processor.
+	sti();
+
+	//loop over process table looking for process with pid
+	acquire(&ptable.lock);
+	cprintf("name \t pid \t state \t \t priority \n");
+	for(p= ptable.proc; p < &ptable.proc[NPROC]; p++){
+		if(p->state == SLEEPING )
+			cprintf("%s \t %d \t SLEEPING \t %d\n ", p->name,p->pid,p->priority);
+		else if(p->state == RUNNING)
+			cprintf("%s \t %d \t RUNNING \t %d\n ", p->name, p->pid,p->priority);
+		else if(p->state == RUNNABLE)
+			cprintf("%s \t %d \t RNNABLE \t %d\n ", p->name, p->pid,p->priority);
+	}
+
+	release(&ptable.lock);
+
+	return 22;
+}
+
+int 
+chpr(int pid,int priority)
+{
+	struct proc*p;
+
+	acquire(&ptable.lock);
+	for(p=ptable.proc; p<&ptable.proc[NPROC];p++){
+		if(p->pid == pid){
+			p->priority = priority;
+			break;
+		}
+	}
+	release(&ptable.lock);
+
+	return pid;
 }
